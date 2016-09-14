eccf3aea4c5bdca2041f959de16201d24948aebd
vicident <tnediciv@gmail.com>
2016-09-14 17:16:28 +0300

    Conv->ConvMM by Toeplitz matrix

--- ios_examples/XOR_Example_LoadPretrained/XOR_Example/xor_lua/johnson.lua
+++ ios_examples/XOR_Example_LoadPretrained/XOR_Example/xor_lua/johnson.lua
@@ -10,12 +10,12 @@ local function res_block()
   local conv_block = nn.Sequential()
   
   conv_block:add(pad(1, 1, 1, 1))
-  conv_block:add(backend.SpatialConvolution(128, 128, 3, 3, 1, 1, 0, 0))
+  conv_block:add(backend.SpatialConvolutionMM(128, 128, 3, 3, 1, 1, 0, 0))
   conv_block:add(normalization(128))
   conv_block:add(backend.ReLU(true))
 
   conv_block:add(pad(1, 1, 1, 1))
-  conv_block:add(backend.SpatialConvolution(128, 128, 3, 3, 1, 1, 0, 0))
+  conv_block:add(backend.SpatialConvolutionMM(128, 128, 3, 3, 1, 1, 0, 0))
   conv_block:add(normalization(128))
 
   local concat = nn.ConcatTable():add(nn.Identity()):add(conv_block)
@@ -35,17 +35,17 @@ if params_mode == 'texture' then
 end
 
 model:add(pad(4, 4, 4, 4))
-model:add(backend.SpatialConvolution(3, 32, 9, 9, 1, 1, 0, 0))
+model:add(backend.SpatialConvolutionMM(3, 32, 9, 9, 1, 1, 0, 0))
 model:add(normalization(32))
 model:add(nn.ReLU(true))
 
 -- probably need replication padding here too
-model:add(backend.SpatialConvolution(32, 64,  3, 3, 2, 2, 1, 1))
+model:add(backend.SpatialConvolutionMM(32, 64,  3, 3, 2, 2, 1, 1))
 model:add(normalization(64))
 model:add(nn.ReLU(true))
 
 -- probably need replication padding here too
-model:add(backend.SpatialConvolution(64, 128, 3, 3, 2, 2, 1, 1))
+model:add(backend.SpatialConvolutionMM(64, 128, 3, 3, 2, 2, 1, 1))
 model:add(normalization(128))
 model:add(nn.ReLU(true))
 
@@ -64,6 +64,6 @@ model:add(normalization(32))
 model:add(nn.ReLU(true))
 
 model:add(pad(1, 1, 1, 1))
-model:add(backend.SpatialConvolution(32, 3, 3, 3, 1, 1, 0, 0))
+model:add(backend.SpatialConvolutionMM(32, 3, 3, 3, 1, 1, 0, 0))
 
 return model:add(nn.TVLoss(params_tv_weight))
